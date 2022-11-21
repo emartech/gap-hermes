077e7a66c943b8f45a11758ef79f7d34ca78582c
Csaba Makara <csaba.makara@emarsys.com>
2022-11-21 11:56:57 +0100

    chore(v2): add validation
    CLOUDP-1538

--- gap-hermes-public-v2.sh
+++ gap-hermes-public-v2.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ -z "$GOOGLE_GHA_CREDS_PATH" ]; then
+    echo "Google credentials are absent. Did you include the checkout and auth steps in your configuration?"
+    exit 1
+fi
+
 export HOME=`pwd`
 
 gcloud auth login --cred-file=$GOOGLE_GHA_CREDS_PATH
